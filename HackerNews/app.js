de81dd9ca69c234d5500e41a3113ea27121de327
tools <1041988365@qq.com>
2017-07-27 22:37:44 +0800

    封装读取 data.json 文件的代码 和 封装获取 post 数据的代码

--- HackerNews/app.js
+++ HackerNews/app.js
@@ -7,6 +7,7 @@ var path=require('path');
 var url=require('url');
 var mime=require('mime');
 var querystring=require("querystring");
+var _=require("underscore");
 
 // 创建服务，并监听request事件
 var server=http.createServer(function(req,res){
@@ -30,14 +31,14 @@ var server=http.createServer(function(req,res){
 	res.setHeader("Content-Type","text/html;charset=utf-8")
 
 	if(reqUrl === "/" || reqUrl ==="/index"){
-
 		res.render(path.join(__dirname,"views","index.html"));
+
 	}else if(reqUrl==="/details"){
-
 		res.render(path.join(__dirname,"views","details.html"));
-	}else if(reqUrl==="/submit"){
 
+	}else if(reqUrl==="/submit"){
 		res.render(path.join(__dirname,"views","submit.html"));
+
 	}else if(reqUrl==="/add"&&req.method.toLowerCase()==="get"){
 
 		// 表示 get 方式提交数据
@@ -46,14 +47,15 @@ var server=http.createServer(function(req,res){
 		// urlObj.query 通过 url 模块解析后，就可以获取 用户 get 提交过来的数据
 
 		// 获取data.json数据，将其转换成list数组
-		fs.readFile(path.join(__dirname,"data","data.json"),'utf8',function(err,data){
-			if(err&&!err.code==="ENOENT"){
-				throw err;
-			}
+		// fs.readFile(path.join(__dirname,"data","data.json"),'utf8',function(err,data){
+		// 	if(err&&!err.code==="ENOENT"){
+		// 		throw err;
+		// 	}
 
-			// 将获取的字符串转换成数组
-			var list = JSON.parse(data||'[]');
+		// 	// 将获取的字符串转换成数组
+		// 	var list = JSON.parse(data||'[]');
 
+		readDataJson(function(list){
 			// 将urlObj.query加入list数组中
 			list.push(urlObj.query);
 
@@ -79,50 +81,51 @@ var server=http.createServer(function(req,res){
 		// 表示 post 方式提交数据
 		// 获取用户提交过来的数据
 
-		fs.readFile(path.join(__dirname,"data","data.json"),'utf-8',function(err,data){
-			if(err&&!err==="ENOENT"){
-				throw err;
-			}else{
-				var list=JSON.parse(data||'[]');
+		// fs.readFile(path.join(__dirname,"data","data.json"),'utf-8',function(err,data){
+		// 	if(err&&!err==="ENOENT"){
+		// 		throw err;
+		// 	}
+		// 	var list=JSON.parse(data||'[]');
 
-				var arr=[];//空数组接收post数据
+		readDataJson(function(list){
+			// var arr=[];//空数组接收post数据
 
-				// 获取post方式的数据，需要监听request对象的data和end事件
-				req.on("data",function(chunk){
-					// 该事件中获取请求的数据是一片一片的提交过来的
-					// chunk是一个二进制对象
-					arr.push(chunk);
-				});
-				req.on("end",function(){
-        			// 当该事件被触发的时候，表示用户数据都接收完毕了
-        			// 把arr数组中的每一个buffer对象拼接起来，最终要生成Buffer对象
-        			var buffer = Buffer.concat(arr);
+			// // 获取post方式的数据，需要监听request对象的data和end事件
+			// req.on("data",function(chunk){
+			// 	// 该事件中获取请求的数据是一片一片的提交过来的
+			// 	// chunk是一个二进制对象
+			// 	arr.push(chunk);
+			// });
+			// req.on("end",function(){
+   //  			// 当该事件被触发的时候，表示用户数据都接收完毕了
+   //  			// 把arr数组中的每一个buffer对象拼接起来，最终要生成Buffer对象
+   //  			var buffer = Buffer.concat(arr);
+   
+   			getPostData(req,function(buffer){
+    			// 把Buffer对象转换为字符串，title=biaoti&url=lujing&text=hahahaha
+    			var reqBody=buffer.toString('utf-8');
 
-        			// 把Buffer对象转换为字符串，title=biaoti&url=lujing&text=hahahaha
-        			var reqBody=buffer.toString('utf-8');
+    			// querystring，通过该模块可以把一个查询字符串转化成json对象
+    			reqBody=querystring.parse(reqBody);
 
-        			// querystring，通过该模块可以把一个查询字符串转化成json对象
-        			reqBody=querystring.parse(reqBody);
+    			// 将新数据加入数据文本中
+    			list.push(reqBody);
 
-        			// 将新数据加入数据文本中
-        			list.push(reqBody);
+    			fs.writeFile(path.join(__dirname,"data","data.json"),JSON.stringify(list),'utf-8',function(err){
+    				if(err){
+    					throw err;
+    				}else{
+    					console.log("写入成功！");
 
-        			fs.writeFile(path.join(__dirname,"data","data.json"),JSON.stringify(list),'utf-8',function(err){
-        				if(err){
-        					throw err;
-        				}else{
-        					console.log("写入成功！");
-
-        					// 执行重定向操作：通过服务器向浏览器响应一个http报文头来实现
-        					// 设置响应状态码
-        					res.statusCode=302;
-        					res.statusMessage="Found";
-        					res.setHeader("Location","/");
-        					res.end();
-        				}
-        			})
-				})
-			}
+    					// 执行重定向操作：通过服务器向浏览器响应一个http报文头来实现
+    					// 设置响应状态码
+    					res.statusCode=302;
+    					res.statusMessage="Found";
+    					res.setHeader("Location","/");
+    					res.end();
+    				}
+    			});
+			});
 		})
 	}else if(reqUrl.startsWith("/resources")){
 
@@ -156,17 +159,35 @@ function render(res){
 	}
 }
 
-// 获取request数据
+// 封装获取 post 数据的代码
 
-// 封装获取data.json数据
+function getPostData(req,callback){
+	var arr=[];//空数组接收post数据
 
-function readDataJson(callBack){
-	fs.readFile(path.join(__dirname,"data","data.json"),"utf-8",function(req,res){
+		// 获取post方式的数据，需要监听request对象的data和end事件
+		req.on("data",function(chunk){
+			// 该事件中获取请求的数据是一片一片的提交过来的
+			// chunk是一个二进制对象
+			arr.push(chunk);
+		});
+
+		req.on("end",function(){
+			// 当该事件被触发的时候，表示用户数据都接收完毕了
+			// 把arr数组中的每一个buffer对象拼接起来，最终要生成Buffer对象
+			var buffer = Buffer.concat(arr);
+
+			callback(buffer);
+		});
+}
+
+// 封装读取 data.json 文件的代码
+
+function readDataJson(callback){
+	fs.readFile(path.join(__dirname,"data","data.json"),"utf-8",function(err,data){
 		if(err&&!err==="ENOENT"){
 			throw err;
-		}else{
-			var list=JSON.parse(data||'[]');
-			callback(list);
 		}
+		var list=JSON.parse(data||'[]');
+		callback(list);
 	})
 }
